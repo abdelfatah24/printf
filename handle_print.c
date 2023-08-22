a5b531fb7e72ac96be2ec6a345d1566077f415be
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-08-22 08:36:12 -0700

    Add handle_print.c file

--- handle_print.c
+++ handle_print.c
@@ -16,11 +16,10 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknow = 0, printed = -1;
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
-		{'c', print_char}, {'d', print_int},
-		{'%', print_percent},
-		{'i', print_int}, {'s', print_string}, {'b', print_binary},
+		{'c', print_char}, {'i', print_int}, {'%', print_percent},
+		{'s', print_string}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
@@ -33,18 +32,21 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
-		{return (-1); }
-		unknow = unknow + write(1, "%%", 1);
+		{	return (-1); }
+		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-		{unknow = unknow + write(1, " ", 1); }
-		else if (width > 0)
-		{(*ind)--;
+		{	unknow_len += write(1, " ", 1); }
+		else if (width)
+		{
+			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
+			{ --(*ind); }
 			if (fmt[*ind] == ' ')
-			{(*ind)--; }
-			return (1); }
-		unknow = unknow + write(1, &fmt[*ind], 1);
-		return ((int)unknow); }
-	return ((int)printed);
+			{	--(*ind); }
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return ((int)unknow_len);
+	}
+	return ((int)printed_chars);
 }
